d4827dc757a571f08bbd8e999ed476fb0d9929df
frqhero <frqhero@gmail.com>
2023-07-26 15:56:48 +0600

    Create a bot and set up logging

--- backend/core/management/commands/run_bot.py
+++ backend/core/management/commands/run_bot.py
@@ -1,15 +1,25 @@
+import logging
 import os
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from telegram.ext import Updater
 import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meetup_management.settings')
 django.setup()
 
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
+
+
 def main():
     tg_bot_token = settings.TG_BOT_TOKEN
+    updater = Updater(token=tg_bot_token)
+    dispatcher = updater.dispatcher
 
 
 class Command(BaseCommand):
