12be865b951544de0bc6d2cc5025d2c67cbde72f
Yaroslav Voroshilov <frqhero@gmail.com>
2023-07-27 23:34:19 +0600

    Carry on creating bot

--- backend/core/management/commands/run_bot.py
+++ backend/core/management/commands/run_bot.py
@@ -4,7 +4,7 @@ import os
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from telegram import Update
-from telegram.ext import Updater, CallbackContext
+from telegram.ext import Updater, CallbackContext, CommandHandler, ConversationHandler, InlineQueryHandler
 import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
@@ -18,7 +18,22 @@ logging.basicConfig(
 
 
 def start(update: Update, context: CallbackContext) -> str:
+    # send a message and then return select_cake_type
     pass
+
+def get_complete_cakes(update: Update, context: CallbackContext) -> str:
+    # send a message and then return select_cake_type
+    pass
+
+def create_custom_cake(update: Update, context: CallbackContext) -> str:
+    # send a message and then return select_cake_type
+    pass
+
+def stop(update: Update, context: CallbackContext) -> int:
+    """End Conversation by command."""
+    update.message.reply_text('Okay, bye.')
+
+    return 'END'
 
 
 def main():
@@ -29,6 +44,16 @@ def main():
     # top level conversation handler
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
+        states={
+            'SELECT_CAKE_TYPE': [InlineQueryHandler(get_complete_cakes), InlineQueryHandler(create_custom_cake)]
+        },
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+    updater.start_polling()
+    updater.idle()
 
 
 class Command(BaseCommand):
